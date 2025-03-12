a4d5437ce2a7c0f5b7ee93262193d19d735e32d9
Chisquare7 <chijiokechibuike.f@gmail.com>
2025-03-12 20:13:45 +0100

    update: set up routing for Login and implemented localStorage for saving data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar.jsx";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
+import Login from "./pages/Login.jsx";
 
 function App() {
-  const [students, setStudents] = useState([]);
+  const [students, setStudents] = useState(() => {
+    return JSON.parse(localStorage.getItem("students")) || [];
+  });
+
+  const [studentUser, setStudentUser] = useState(() => {
+    return JSON.parse(localStorage.getItem("studentUser")) || null;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("students", JSON.stringify(students));
+  }, [students]);
+
+  useEffect(() => {
+    localStorage.setItem("studentUser", JSON.stringify(studentUser));
+  }, [studentUser]);
 
   return (
     <>
@@ -13,6 +28,10 @@ function App() {
         <NavBar />
         <Routes>
           <Route path="/" element={<Home students={students} />} />
+          <Route
+            path="/login"
+            element={<Login setStudentUser={setStudentUser} />}
+          />
           <Route
             path="/signup"
             element={<Signup students={students} setStudents={setStudents} />}
